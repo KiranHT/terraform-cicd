15c37846c7558e95c4a9733081b34f059933949a
KiranHT <108686865+KiranHT@users.noreply.github.com>
2023-06-19 11:37:51 +0530

    only allowed the features block at provider.tf file

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "2f194eac-d198-4087-a11d-b84257a40f66"
-  tenant_id = "edcef96e-1fd5-40df-ba57-1b4f2823db97"
-  client_id = "3b815dfe-62e4-40fe-ac7b-ad86ec28c5af"
-  client_secret= "JT_8Q~KMGu2-rifIu2edg9RhhHbI3c~jcLdnbc6C"
+  //subscription_id = "2f194eac-d198-4087-a11d-b84257a40f66"
+  //tenant_id = "edcef96e-1fd5-40df-ba57-1b4f2823db97"
+  //client_id = "3b815dfe-62e4-40fe-ac7b-ad86ec28c5af"
+  //client_secret= "JT_8Q~KMGu2-rifIu2edg9RhhHbI3c~jcLdnbc6C"
   features {}
   
 }
