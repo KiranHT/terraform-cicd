c8c092c1a099e3e5f80c034d2047ecc46d7bd958
KiranHT <108686865+KiranHT@users.noreply.github.com>
2023-06-19 10:39:43 +0530

    Update main.tf
    update the main.tf file

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.53.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  subscription_id = "2f194eac-d198-4087-a11d-b84257a40f66"
-  tenant_id = "edcef96e-1fd5-40df-ba57-1b4f2823db97"
-  client_id = "3b815dfe-62e4-40fe-ac7b-ad86ec28c5af"
-  client_secret= "JT_8Q~KMGu2-rifIu2edg9RhhHbI3c~jcLdnbc6C"
-  features {}
-  
-}
-
-
 resource "azurerm_resource_group" "rg" {
   name     = "testing-rg"
   location = "Central India"
