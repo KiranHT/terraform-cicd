0038b9f414212c304274bd701f20f87cf98e1584
KiranHT <108686865+KiranHT@users.noreply.github.com>
2023-07-10 14:53:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,10 +14,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "2f194eac-d198-4087-a11d-b84257a40f66"
-  tenant_id = "edcef96e-1fd5-40df-ba57-1b4f2823db97"
-  client_id = "c79463a8-cce1-40b8-aea8-c35b9aea1187"
-  client_secret= "5w78Q~YCDFTHSwKJ62cjE7y5h3.OgrSmENe-bcMT"
+ 
   features {}
   
 }
